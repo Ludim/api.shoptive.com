e90ec0bcac4e535f29ced709e3ef11b41bb91358
Ludim <ludim.anel@gmail.com>
2016-02-01 16:58:58 -0600

    get latitude and longitude

--- v1/rest.php
+++ v1/rest.php
@@ -1,9 +1,3 @@
-<html>
-<body bgcolor="#E6E6FA">
-<h1>Shoptive!</h1>
-
-</body>
-</html> 
 <?php
 require_once 'api.php';
 //use Api\Shoptive;
@@ -12,25 +6,75 @@ class Rest extends Api {
     {
         parent::__construct();
     }
-    public function resourceExists($request)
-    {
-        echo $request;
-        $existent_resources = array('getStores', 'getClothes');
-        if (!in_array(array_shift($request), $existent_resources)) {
-            return 0;
-        }
-    }
     public function getRequestData()
     {
-        return parent::getRequestData();
+        $request = parent::getRequestData();
+        $resource = $this->resourceExists($request);
+        //$function_name = "$this->".$resource[0]."()";
+        $object_method_name = strtolower(array_shift($resource));
+        $object_method_args = $resource;
+        if($this->methodExistsInsideThisClass($object_method_name) > 0) {
+            if($this->arraySize($object_method_args) > 0) {
+                call_user_func_array(array($this, $object_method_name),$object_method_args);
+            } else {
+                call_user_func(array($this, $object_method_name));
+            }
+        } else {
+            parent::response($response_code = 405, "Method Not Exists");
+        }
     }
+    public function resourceExists($request)
+    {
+        $existent_resources = array('getStores', 'getClothes',
+                                    'getStoresByZipCode');
+        if($request[0] == "v1") {
+            array_shift($request);
+        }
+        if (!in_array($request[0], $existent_resources)) {
+            //echo "no existe\n";
+            parent::response($response_code = 405,"Resource does not exist");
+            exit;
+        }
+        return $request;
+    }
+
+    public function methodExistsInsideThisClass($method_name)
+    {
+        return (int) method_exists($this, $method_name);
+    }
+
+    public function arraySize($array)
+    {
+        return count($array);
+    }
+
+    public function getStores($coordinate)
+    {
+        $latitude = $this->getLatitude($coordinate);
+        $longitude = $this->getLongitude($coordinate);
+
+        $c = array('coordinate' => array('latitude' => $latitude, 'longitude'=>$longitude ));
+        parent::response($response_code = 200, $c);
+    }
+
+    public function getLatitude($coordinate)
+    {
+        preg_match('/=(.*?),/', $coordinate, $output);
+        return $output[1];
+    }
+
+    public function getLongitude($coordinate)
+    {   
+        preg_match('/,(.*?)$/', $coordinate, $output);
+        return $output[1];
+    }
+
+    public function getClothes($clothes)
+    {
+        echo "getClothes";
+    }
+
 }
 
-#$_SERVER["REQUEST_METHOD"] = "GET";
-#$_GET["PATH_INFO"] = "getStores/coordinate=123.123456,-92.212348";
 $rest = new Rest();
-$user_request = $rest->getRequestData();
-print_r($user_request);
-//$resourceExists = $rest->resourceExists();
-//$rest->resourceExists($_GET["PATH_INFO"]);
-?>
+$rest->getRequestData();
